987daac84d9895e8b5a9f966eb37847e2d40f7c6
Srishti Mishra <55997954+srishti-coder@users.noreply.github.com>
2022-06-15 19:43:04 +0530

    Delete clone stck.cpp

--- clone stck.cpp
+++
@@ -1,8 +0,0 @@
-https://practice.geeksforgeeks.org/problems/clone-a-stack-without-usinig-extra-space/1
-class Solution{
-    public:
-    void clonestack(stack<int> st, stack<int>& cloned)
-    {
-      cloned.swap(st);  //code here
-    }
-};
