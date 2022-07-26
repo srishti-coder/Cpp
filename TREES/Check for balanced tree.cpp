acc5178207327c5b4505261c18aaee5a66284adb
Srishti Mishra <55997954+srishti-coder@users.noreply.github.com>
2022-07-26 23:29:08 +0530

    Update Check for balanced tree.cpp

--- TREES/Check for balanced tree.cpp
+++ TREES/Check for balanced tree.cpp
@@ -114,8 +114,8 @@ class Solution{
             return p;
         }
         
-        pair<int,int> left = isBalancedFast(root->left);
-        pair<int,int> right = isBalancedFast(root->right);
+        pair<bool,int> left = isBalancedFast(root->left);
+        pair<bool,int> right = isBalancedFast(root->right);
         
         
         bool leftAns = left.first;
